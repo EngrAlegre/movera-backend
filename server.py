c14b225febda195488b035106143903698a3fff9
EngrAlegre <qjisalegre@tip.edu.ph>
2026-03-11 23:46:36 +0800

    Fix strip_markdown bug: re.DOTALL passed as string instead of flag

--- server.py
+++ server.py
@@ -133,7 +133,7 @@ def strip_markdown(text: str) -> str:
     text = re.sub(r'\*(.+?)\*', r'\1', text)
     text = re.sub(r'_(.+?)_', r'\1', text)
     text = re.sub(r'~~(.+?)~~', r'\1', text)
-    text = re.sub(r'`{1,3}(.+?)`{1,3}', r'\1', re.DOTALL)
+    text = re.sub(r'`{1,3}(.+?)`{1,3}', r'\1', text, flags=re.DOTALL)
     text = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', text)
     text = re.sub(r'^>\s?', '', text, flags=re.MULTILINE)
     text = re.sub(r'^[-*+]\s', '', text, flags=re.MULTILINE)
